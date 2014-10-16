7558f4637ca55f58482a658d69a4a31ccbe37b91
sean wang <xiaohang.wang.th@gmail.com>
2014-10-15 23:18:31 -0400

    v2

--- milestone1/quaries.sql
+++ milestone1/quaries.sql
@@ -1,14 +1,22 @@
---1. For what percentage of all times has the court favored parties from NY?
+--1. Find all judges who work at New York
 
+SELECT name
+FROM Judge j, Court c
+WHERE j.court_id=c.court_id AND c.city='NY';
+
+--2. Find all the cases that parties from New York have involved
 CREATE VIEW NYPartyInvolved AS
 SELECT I.serial AS serial, I.party_id AS party_id, I.status AS status
 FROM Involved AS I, Party AS P
 WHERE P.location = 'NY' AND P.party_id = I.party_id;
 
+--3. Find all the cases that parties from New York have involved and won
 CREATE VIEW NYPartyWin AS
 SELECT NY.serial AS serial, NY.party_id AS party_id, NY.status AS status
 FROM Cases AS C, NYPartyInvolved AS NY
 WHERE C.serial = NY.serial AND C.result = NY.status;
+
+--4. For what percentage of all times has the court favored parties from NY?
 
 SELECT 100*COUNT(*)/(SELECT COUNT(*) FROM NYPartyInvolved)
 FROM NYPartyWin;
@@ -16,7 +24,21 @@ FROM NYPartyWin;
 DROP VIEW NYPartyInvolved;
 DROP VIEW NYPartyWin;
 
---2. Does Judge Alan Lourie favor defendant or plaintiff more in bio-related cases? 
+--5. Find all specific area(bio-related) cases that a specific(Alan Lourie) has favored the defendant
+
+SELECT C.serial AS serial
+FROM Judge AS J, Cases AS C, Rules AS R
+WHERE J.name = 'Alan Lourie' AND J.judge_id = R.judge_id
+AND r.serial = C.serial AND C.technical_area = 'bio' AND R.decision = 'def';
+
+--5. Find all specific area(bio-related) cases that a specific(Alan Lourie) has favored the plaintiff
+
+SELECT C.serial AS serial
+FROM Judge AS J, Cases AS C, Rules AS R
+WHERE J.name = 'Alan Lourie' AND J.judge_id = R.judge_id
+AND r.serial = C.serial AND C.technical_area = 'bio' AND R.decision = 'plt';
+
+--7. Does Judge Alan Lourie favor defendant or plaintiff more in bio-related cases? 
 
 CREATE VIEW ALFavorDef AS
 SELECT C.serial AS serial
@@ -36,7 +58,14 @@ FROM ALFavorDef;
 DROP VIEW ALFavorDef;
 DROP VIEW ALFavorPlt;
 
---3. The current client is the plaintiff, the issue of the case is "utility," which judge will most likely support the client's position? 
+--8. For every judge who has ever supported a plaintiff on cases of "utility" issue, find the number of the cases
+
+SELECT R.judge_id AS judge_id, COUNT(*) AS cnt
+FROM Cases AS C, Rules AS R 
+WHERE C.issue = 'utility' AND C.serial = R.serial AND R.decision = 'plt'
+GROUP BY R.judge_id;
+
+--9. The current client is the plaintiff, the issue of the case is "utility," which judge will most likely support the client's position? 
 
 CREATE VIEW Potential AS
 SELECT R.judge_id AS judge_id, COUNT(*) AS cnt
@@ -50,7 +79,9 @@ WHERE P1.judge_id = J.judge_id
 AND (P1.cnt >= (SELECT MAX(P2.cnt) FROM Potential AS P2));
                      
 DROP VIEW Potential;
---4. Is being a female a disadvantage litigating in the Federal Circuit? 
-select 100*count(*)/(select count(*) from Party p1, Involved i1 where p1.gender='female' AND i1.party_id=p1.party_id)
-from Party p, Involved i, Cases c
-where p.gender='female' AND i.serial=c.serial AND i.party_id=p.party_id AND c.result=i.status;
+
+--10. Is being a female a disadvantage litigating in the Federal Circuit? 
+
+SELECT 100*count(*)/(select count(*) from Party p1, Involved i1 where p1.gender='female' AND i1.party_id=p1.party_id)
+FROM Party p, Involved i, Cases c
+WHERE p.gender='female' AND i.serial=c.serial AND i.party_id=p.party_id AND c.result=i.status;
