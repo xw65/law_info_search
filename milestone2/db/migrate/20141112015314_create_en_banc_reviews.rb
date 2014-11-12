d6dde37b70653a095609042bd89b953c4cbab96b
JunLyu <junlyu1990@gmail.com>
2014-11-11 21:00:06 -0500

    v3

--- milestone2/db/migrate/20141112015314_create_en_banc_reviews.rb
+++ milestone2/db/migrate/20141112015314_create_en_banc_reviews.rb
@@ -1,7 +1,8 @@
 class CreateEnBancReviews < ActiveRecord::Migration
   def change
     create_table :en_banc_reviews do |t|
-      
+      t.integer :case_id
+      t.integer :case_id
       t.timestamps
     end
   end
